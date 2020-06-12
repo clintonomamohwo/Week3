ac87fc593a74702b4cffeeb10a05ef2b231b1334
Ruona Clinton <66035235+clintonomamohwo@users.noreply.github.com>
2020-06-11 22:05:24 -0400

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -14,8 +14,10 @@ public class Week3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // TODO code application logic here
+        System.out.println("Getting better")
     }
     
 }
